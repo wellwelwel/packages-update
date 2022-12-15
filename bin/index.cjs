b8eeb9019501783a5ab4a4f6e6f37dbac97bb2f6
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2022-12-15 02:54:38 -0300

    Showing current package in realtime

--- bin/index.cjs
+++ bin/index.cjs
@@ -11,8 +11,20 @@ const getVersionBy = require('../src/get-versions.cjs');
    const packageFile = readFileSync(path, 'utf-8');
    const packageJSON = JSON.parse(packageFile);
    const { dependencies, devDependencies } = packageJSON;
+   const totalDependencies =
+      (dependencies ? Object?.keys(dependencies)?.length || 0 : 0) +
+         (devDependencies ? Object?.keys(devDependencies)?.length || 0 : 0) || 0;
    const hasUpdate = [];
    const hasError = [];
+
+   let largestPackageName = '';
+   let count = 0;
+
+   if (!totalDependencies || totalDependencies === 0) {
+      log('No dependencies in package.json 👾');
+
+      return;
+   }
 
    const option =
       args?.[0] && getVersionBy?.[args?.[0]?.replace(/^--/, '')] ? args?.[0]?.replace(/^--/, '') : 'latest' || 'latest';
@@ -21,6 +33,15 @@ const getVersionBy = require('../src/get-versions.cjs');
       const dependencyType = dependencies?.[dependency] ? dependencies : devDependencies;
       const currentVersion = dependencyType[dependency];
       const operators = currentVersion.replace(/[^^><=~]+/g, '');
+
+      if (dependency.length > largestPackageName.length) largestPackageName = dependency;
+
+      process.stdout.write(
+         `\x1b[0G🔎 ${String(++count).padStart(
+            String(totalDependencies).length,
+            ' '
+         )}/${totalDependencies}   ${dependency.padEnd(largestPackageName.length, ' ')}`
+      );
 
       if (option === 'lock') {
          dependencyType[dependency] = currentVersion.replace(/[^a-z0-9.-]/gi, '');
@@ -59,10 +80,18 @@ const getVersionBy = require('../src/get-versions.cjs');
    for (const dependency in dependencies) await compareVersions(dependency);
    for (const dependency in devDependencies) await compareVersions(dependency);
 
+   process.stdout.write(
+      `\x1b[0G🔎 ${String(count).padStart(String(totalDependencies).length, ' ')}/${totalDependencies} ✅${''.padEnd(
+         largestPackageName.length,
+         ' '
+      )}`
+   );
+   log('\n');
+
    writeFileSync(path, `${JSON.stringify(packageJSON, null, 3)}${EOL}`);
 
    hasError.length > 0 && hasUpdate.length > 0 && log();
-   hasUpdate.length > 0 ? showUpdated(hasUpdate) : log(`\nNothing to be updated ✅\n`);
+   hasUpdate.length > 0 ? showUpdated(hasUpdate) : log(`Nothing to be updated ✅\n`);
 
    process.exit(hasError.length > 0 ? 1 : 0);
 })();
